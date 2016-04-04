e8fdeb956447071ced8262ee3ad9ffe32f2704c6
bruno <bruno@administrateur-HP-ProDesk-600-G1-TWR.(none)>
2016-04-04 10:03:40 +0200

    version initiale du projet

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# Projet_afficheur_hack_spark" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Guilbertb/Projet_afficheur_hack_spark.git
+git push -u origin master
